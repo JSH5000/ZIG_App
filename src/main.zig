9e6fe2181dc1bb7c1483560e79abc33b80bf3b02
JSH5000 <39937127+JSH5000@users.noreply.github.com>
2023-08-04 16:27:41 -0500

    Altered console to test IO functionality

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,18 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("Hello, world!", .{});
+    const in = std.io.getStdIn();
+    var buf = std.io.bufferedReader(in.reader());
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var r = buf.reader();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("Enter something: ", .{});
 
-    try bw.flush(); // don't forget to flush!
-}
+    var msg_buf: [4096]u8 = undefined;
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var msg = try r.readUntilDelimiterOrEof(&msg_buf, '\n');
+
+    if (msg) |m| {
+        std.debug.print("You entered: {s}\n", .{m});
+    }
 }
